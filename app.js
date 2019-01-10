71e49983047e9d6cdce3389d09495509ac1088d1
KevinFehrenbach17 <x2g1cx@gmail.com>
2019-01-10 01:23:06 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+function myFunction(x) {
+  x.classList.toggle("change");
+  document.getElementById("myDropdown").classList.toggle("show");
+
+}
+
+
+
+// particlesJS
+
+// particlesJS.load('particles-js',
+// 'particles.json',function(){
+//   console.log('particles.json loaded..')
+// });
